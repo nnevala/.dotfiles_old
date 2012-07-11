bc78189d50b17b49a57c12d3befb56e7ba38a58f
Niko Nevala <nnevala@inviqa.com>
2012-07-11 15:25:14 +0100

    Rename source files to reflect actual configuration files

--- update.sh
+++ update.sh
@@ -3,21 +3,21 @@
 git pull
 
 # bash
-grep ".dotfiles/src/bash" ../.bash_profile > /dev/null
+grep ".dotfiles/src/.bash_profile" ../.bash_profile > /dev/null
 
 if [ $? -gt 0 ]
 then
 	echo "Linking bash..."
-	echo "\n# The following line(s) were added by .dotfiles/update.sh\nsource .dotfiles/src/bash" >> ../.bash_profile
+	echo "\n# The following line(s) were added by .dotfiles/update.sh\nsource .dotfiles/src/.bash_profile" >> ../.bash_profile
 fi
 
 # git
-grep ".dotfiles/src/git" ../.gitconfig > /dev/null
+grep ".dotfiles/src/.gitconfig" ../.gitconfig > /dev/null
 
 if [ $? -gt 0 ]
 then
 	echo "Linking git..."
-	echo "\n# The following line(s) were added by .dotfiles/update.sh\n[include]\n\tpath = .dotfiles/src/git" >> ../.gitconfig
+	echo "\n# The following line(s) were added by .dotfiles/update.sh\n[include]\n\tpath = .dotfiles/src/.gitconfig" >> ../.gitconfig
 fi
 
 echo "All done"
