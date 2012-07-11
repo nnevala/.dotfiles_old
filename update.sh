0b7ce810ebf9ce248c99ea396242a2546bb497d8
Niko Nevala <nnevala@inviqa.com>
2012-07-11 15:18:28 +0100

    Add explanatory comments to config files

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ grep ".dotfiles/src/bash" ../.bash_profile > /dev/null
 if [ $? -gt 0 ]
 then
 	echo "Linking bash..."
-	echo "\nsource .dotfiles/src/bash" >> ../.bash_profile
+	echo "\n# The following line(s) were added by .dotfiles/update.sh\nsource .dotfiles/src/bash" >> ../.bash_profile
 fi
 
 # git
@@ -17,7 +17,7 @@ grep ".dotfiles/src/git" ../.gitconfig > /dev/null
 if [ $? -gt 0 ]
 then
 	echo "Linking git..."
-	echo "\n[include]\n\tpath = .dotfiles/src/git" >> ../.gitconfig
+	echo "\n# The following line(s) were added by .dotfiles/update.sh\n[include]\n\tpath = .dotfiles/src/git" >> ../.gitconfig
 fi
 
 echo "All done"
