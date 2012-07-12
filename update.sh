c158ac8e53d7f1b3a87c9b02df28bac28fba5a80
Niko Nevala <nnevala@inviqa.com>
2012-07-12 17:47:29 +0100

    Change src to conf

--- update.sh
+++ update.sh
@@ -3,21 +3,21 @@
 git pull
 
 # bash
-grep ".dotfiles/src/.bash_profile" ../.bash_profile > /dev/null
+grep ".dotfiles/conf/.bash_profile" ../.bash_profile > /dev/null
 
 if [ $? -gt 0 ]
 then
 	echo "Linking bash..."
-	echo "\n# The following line(s) were added by .dotfiles/update.sh\nsource .dotfiles/src/.bash_profile" >> ../.bash_profile
+	echo "\n# The following line(s) were added by .dotfiles/update.sh\nsource .dotfiles/conf/.bash_profile" >> ../.bash_profile
 fi
 
 # git
-grep ".dotfiles/src/.gitconfig" ../.gitconfig > /dev/null
+grep ".dotfiles/conf/.gitconfig" ../.gitconfig > /dev/null
 
 if [ $? -gt 0 ]
 then
 	echo "Linking git..."
-	echo "\n# The following line(s) were added by .dotfiles/update.sh\n[include]\n\tpath = .dotfiles/src/.gitconfig" >> ../.gitconfig
+	echo "\n# The following line(s) were added by .dotfiles/update.sh\n[include]\n\tpath = .dotfiles/conf/.gitconfig" >> ../.gitconfig
 fi
 
 echo "All done"
